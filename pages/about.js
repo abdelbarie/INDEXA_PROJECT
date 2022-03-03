06e6f83167f8b868dc905a9683eaf139ed9b12ed
ouaelesi <ouael2019esi@gmail.com>
2022-03-03 14:03:58 +0100

    change the about us page

--- pages/about.js
+++ pages/about.js
@@ -13,27 +13,37 @@ const About = () => {
 
 
   return (
-    <div className={`${locale === 'fr' ? 'text-left' : 'text-right'} md:px-16 lg:px-36 pb-20 pt-10`}>
-      <div className=" text-white md:gap-0 rounded-2xl shadow-md mt-8  relative md:px-10 py-5 px-7 w-full mx-auto md:w-10/12">
-        <span className="text-3xl text-custom-green font-semibold">{content.aboutUS.title}</span>
+    <div className={`${locale === 'fr' ? 'text-left' : 'text-right'} md:px-16 lg:px-36 pb-20 pt-5 `}>
+      <div className=" dark:text-white md:gap-0 rounded-2xl mt-8  relative md:px-10 py-5 px-4 w-full mx-auto md:w-10/12">
+        <span className=" text-3xl text-custom-green font-semibold">{content.aboutUS.title}</span>
+        <hr/>
         <div className='text-md productDetails mt-2 font-light'>
         {content.aboutUS.description}  
          </div>
-        <img src="/imgs/greenCircle.svg" className="absolute top-32 -right-6 w-12"></img>
-        <img src="/imgs/smallCircles.svg" className="absolute -bottom-6 -left-6 w-16"></img>
+        {/* <img src="/imgs/greenCircle.svg" className="absolute top-32 -right-6 w-12"></img>
+        <img src="/imgs/smallCircles.svg" className="absolute -bottom-6 -left-6 w-16"></img> */}
       </div>
 
-      <div className=" text-white md:gap-0 rounded-2xl shadow-md mt-8  relative md:px-10 py-5 px-7 w-full mx-auto md:w-10/12">
-        <span className="text-3xl text-custom-green font-semibold">{content.NosRubrique.title}</span>
+      <div className=" dark:text-white md:gap-0 rounded-2xl mt-8  relative md:px-10 py-5 px-4 w-full mx-auto md:w-10/12">
+        <span className=" text-3xl text-custom-green font-semibold">{content.NosRubrique.title}</span>
+        <hr/>
         <div className='text-md productDetails mt-2 font-light'>
         {content.NosRubrique.content1}  
          </div>
          <div className='text-md productDetails mt-2 font-light'>
         {content.NosRubrique.content2}  
          </div>
-        <img src="/imgs/greenCircle.svg" className="absolute -bottom-6 -left-6  w-12"></img>
-        <img src="/imgs/smallCircles.svg" className="absolute top-32 -right-6 w-16"></img>
+        {/* <img src="/imgs/greenCircle.svg" className="absolute -bottom-6 -left-6  w-12"></img>
+        <img src="/imgs/smallCircles.svg" className="absolute top-32 -right-6 w-16"></img> */}
+        <div className="flex gap-1 mt-4">
+          <img src="/imgs/marque1.jpg" width="20%"></img>
+          <img src="/imgs/marque2.jpg" width="20%"></img>
+          <img src="/imgs/marque3.jpg" width="37%"></img>
+          <img src="/imgs/marque4.jpg" width="20%"></img>
+
+        </div>
       </div>
+    
     </div>
   );
 };
