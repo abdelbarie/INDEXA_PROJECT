18c3f4a5d427260e3dd09592e1de640b207b55a0
ouaelesi <ouael2019esi@gmail.com>
2022-03-07 03:10:18 +0100

    legandry final changes

--- components/partials/HomePageHeader.js
+++ components/partials/HomePageHeader.js
@@ -4,8 +4,9 @@ import Link from 'next/link';
 const HomePageHeader = (props) => {
     return (
         <div className="HomePgeHeader relative bg-white md:text-left grid md:grid-cols-2 px-10 md:px-20 lg:px-32 text-custom-dark-blue dark:text-custom-dark-bg" style={{backgroundImage:`url(${props.backGround})`}}>
+          <div className="absolute w-full h-full bg-custom-dark-green opacity-50 dark:block hidden"></div>
         <div className="h-fit my-auto relative">
-          <div className={`${props.greentext ? 'text-custom-green' : ''} font-extrabold text-4xl drop-shadow-md lg:text-6xl py-3`}>
+          <div className={`${props.greentext ? 'text-custom-green' : ''}  frop-shadow-md font-extrabold text-4xl drop-shadow-md lg:text-6xl py-3`}>
             {props.content.home.header.title}
           </div>
           <div className={`${props.greentext ? 'text-white' : ''} font-semibold md:text-lg text-base  drop-shadow-md`}>
